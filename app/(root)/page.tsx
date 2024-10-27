61a7791aed03d005b403f4f0519cc55279ecd2f7
Abdul Muhaymin <abood.olabi2002@gmail.com>
2024-10-28 00:12:17 +0300

    changes updated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,34 +1,44 @@
 import Navbar from "@/components/Navbar";
 import SearchForm from "@/components/SearchForm";
 
-export default async function Home({ searchParams } : {
-    searchParams: Promise<{query?:string}>}) {
+export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
+    const query = (await searchParams).query;
 
-    const query = (await searchParams).query
+    const posts = [{
+        _createdAt: 'Yesterday',
+        views: 55,
+        author: {_id: 1},
+        _id: 1,
+        description: 'This is a description',
+        image: '',
+        category: 'Robots',
+        title: 'We Robots'
+    }]
 
     return (
         <>
-            <section
-                className='pink_container'
-            >
-                <h1
-                    className='heading'
-                >
+            <section className="pink_container">
+                <h1 className="heading">
                     Pitch Your Startup,
-                    <br/>
+                    <br />
                     Connect With Entrepreneurs
                 </h1>
-                <p
-                className='sub-heading !max-w-3xl' // ! we use it in order when we want to overwrite some other styles previously provided to this element
-                >
+                <p className="sub-heading !max-w-3xl">
                     Submit Ideas, Vote On Pitches & Get Noticed In Virtual Competitions.
                 </p>
 
-                <SearchForm
-                    query={query}
-                />
+                <SearchForm query={query} />
             </section>
 
+            <section className="section_container">
+                <p className="text-30-semibold">
+                    {query ? `Search results for "${query}"` : "All Startups"}
+                </p>
+
+                <ul className='mt-7 card_grid'>
+
+                </ul>
+            </section>
         </>
-    )
+    );
 }
